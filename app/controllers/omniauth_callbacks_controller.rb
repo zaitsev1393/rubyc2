363cf030bef29b1bdb6d9b22f9a3fa921def4c96
zaitsev1393 <zaitsev1393@gmail.com>
2016-12-02 11:55:14 +0200

    pre rspec commit

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,23 +1,22 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    #render :text => request.env['omniauth.auth'].to_yaml
-    sign_in_and_redirect @user
-  end
-
-  def twitter
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    #render :text => request.env['omniauth.auth'].to_yaml
-    sign_in_and_redirect @user
-    p @user
+    social
   end
 
   def vkontakte
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    #render :text => request.env['omniauth.auth'].to_yaml
-    sign_in_and_redirect @user
-    p @user
+    social
+  end
+
+  def twitter
+    social
   end
   
+private
+
+  def social
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
+  end
+
 end
